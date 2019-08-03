ba68a8541a5f200419c5c836a816202c07cd9629
Nick Ingolia <nick@ingolia.org>
2019-08-02 19:12:53 -0700

    Changed to use BAM format alignments

--- src/bc-pacbio/bc-pbj.rs
+++ src/bc-pacbio/bc-pbj.rs
@@ -1,14 +1,13 @@
 extern crate barcode_assign;
 extern crate failure;
-//extern crate rust_htslib;
+extern crate rust_htslib;
 
 use std::cmp::*;
 use std::collections::HashMap;
 use std::io::{BufRead,BufReader,Write};
 
-use barcode_assign::blasr::*;
-//use rust_htslib::bam;
-//use rust_htslib::prelude::*;
+use rust_htslib::bam;
+use rust_htslib::prelude::*;
 
 fn main() {
     pacbio_join().unwrap_or_else(|err| {
@@ -38,13 +37,17 @@ fn pacbio_join() -> Result<(), failure::Error> {
         }
     }
 
-    let aligns_in = BufReader::new(std::fs::File::open("frags_aligned.txt")?);
-    for line_res in aligns_in.lines() {
-        let line = line_res?;
-        let blasr = BlasrAlign::new(&line)?;
-        let read = blasr.query_name_trimmed().to_string();
+    let mut aligns_in = bam::Reader::from_path(&"frags_aligned.bam")?;
+    let target_names_res: Result<Vec<_>, _>
+        = aligns_in.header().target_names().into_iter().map(|t| String::from_utf8(t.to_vec())).collect();
+    let target_names = target_names_res?;
+    for res in aligns_in.records() {
+        let r = res?;
+        let qname = String::from_utf8(r.qname().to_vec())?;
+
+        let read = &qname[0..qname.rfind("/").unwrap_or(qname.len())];
         let aligns = read_to_aligns.entry(read.to_string()).or_insert(Vec::new());
-        aligns.push(blasr);
+        aligns.push(r);
     }
 
     let mut good_out = std::fs::File::create("barcode-aligns-good.txt")?;
@@ -54,7 +57,7 @@ fn pacbio_join() -> Result<(), failure::Error> {
         if let Some(aligns) = read_to_aligns.get(read) {
             if aligns.len() == 1 {
                 write!(all_out, "{}\t{}\tUnique\n", barcode, library)?;
-                write!(good_out, "{}\t{}\t{}\n", barcode, library, aligns[0])?;
+                write!(good_out, "{}\t{}\t{}\n", barcode, library, format_align(&target_names, &aligns[0])?)?;
             } else {
                 write!(all_out, "{}\t{}\tMulti\t{}\n", barcode, library, aligns.len())?;
             }
@@ -67,24 +70,23 @@ fn pacbio_join() -> Result<(), failure::Error> {
     let mut barcode_out = std::fs::File::create("barcode-assign-all.txt")?;
 
     for (&(ref library, ref barcode), &ref reads) in barcode_to_reads.iter() {
-        let aligns: Vec<Vec<BlasrAlign>> = reads.iter().map(|r| read_to_aligns.get(r).map_or(Vec::new(), |alns| alns.clone())).collect();
+        let aligns: Vec<Vec<bam::Record>> = reads.iter().map(|r| read_to_aligns.get(r).map_or(Vec::new(), |alns| alns.clone())).collect();
 
         if aligns.iter().any(|read_align| read_align.len() > 1) {
             write!(barcode_out, "{}\t{}\t{}\tMulti\n", library, barcode, reads.len())?;
             continue;
         }
 
-        let read_aligns: Vec<&BlasrAlign> = aligns.iter().filter_map(|read_align| read_align.first()).collect();
+        let read_aligns: Vec<&bam::Record> = aligns.iter().filter_map(|read_align| read_align.first()).collect();
         
         if read_aligns.len() == 0 {
             write!(barcode_out, "{}\t{}\t{}\tNone\n", library, barcode, reads.len())?;
             continue;            
         }
 
-        if read_aligns.iter().all(|ra| ra.target_name() == read_aligns[0].target_name()
-                                  && ra.target_start() == read_aligns[0].target_start()
-                                  && ra.target_end() == read_aligns[0].target_end()) {
-            write!(assign_out, "{}\t{}\t{}\t{}\n", library, barcode, reads.len(), read_aligns[0])?;
+        if read_aligns.iter().all(|ra| ra.tid() == read_aligns[0].tid()
+                                  && ra.pos() == read_aligns[0].pos()) {
+            write!(assign_out, "{}\t{}\t{}\t{}\n", library, barcode, reads.len(), format_align(&target_names, read_aligns[0])?)?;
             write!(barcode_out, "{}\t{}\t{}\tGood\n", library, barcode, reads.len())?;
         } else {
             write!(barcode_out, "{}\t{}\t{}\tMixed\n", library, barcode, reads.len())?;
@@ -93,4 +95,12 @@ fn pacbio_join() -> Result<(), failure::Error> {
     }
 
     Ok(())
+}
+
+fn format_align(names: &Vec<String>, r: &bam::Record) -> Result<String, failure::Error> {
+    Ok(format!("{}\t{}\t{}\t{}",
+               names[r.tid() as usize],
+               r.pos(),
+               r.cigar().end_pos()?,
+               if r.is_reverse() { "-" } else { "+" } ))
 }
