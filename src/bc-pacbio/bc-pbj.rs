b18d48de6335a67fb63a4ec45af556531f225e3f
Nick Ingolia <nick@ingolia.org>
2019-08-03 12:06:48 -0700

    Better handling of unambiguous multi-mapped reads

--- src/bc-pacbio/bc-pbj.rs
+++ src/bc-pacbio/bc-pbj.rs
@@ -2,7 +2,6 @@ extern crate barcode_assign;
 extern crate failure;
 extern crate rust_htslib;
 
-use std::cmp::*;
 use std::collections::HashMap;
 use std::io::{BufRead, BufReader, Write};
 
@@ -23,7 +22,7 @@ fn pacbio_join() -> Result<(), failure::Error> {
     let mut read_to_aligns = HashMap::new();
     let mut barcode_to_reads = HashMap::new();
 
-    let barcodes_in = BufReader::new(std::fs::File::open("barcodes.txt")?);
+    let barcodes_in = BufReader::new(std::fs::File::open("read-inserts-good.txt")?);
     for line_res in barcodes_in.lines() {
         let line = line_res?;
         let fields: Vec<&str> = line.split("\t").collect();
@@ -32,13 +31,13 @@ fn pacbio_join() -> Result<(), failure::Error> {
         } else {
             if let Some(_prev) = read_to_barcode.insert(
                 fields[0].to_string(),
-                (fields[1].to_string(), fields[3].to_string()),
+                (fields[3].to_string(), fields[1].to_string()),
             ) {
                 println!("Duplicated read name {:?}", line);
             }
 
             let reads = barcode_to_reads
-                .entry((fields[1].to_string(), fields[3].to_string()))
+                .entry((fields[3].to_string(), fields[1].to_string()))
                 .or_insert(Vec::new());
             reads.push(fields[0].to_string());
         }
@@ -61,38 +60,36 @@ fn pacbio_join() -> Result<(), failure::Error> {
         aligns.push(r);
     }
 
-    let mut good_out = std::fs::File::create("barcode-aligns-good.txt")?;
-    let mut all_out = std::fs::File::create("barcode-aligns-all.txt")?;
-
-    for (&ref read, &(ref library, ref barcode)) in read_to_barcode.iter() {
+    for ref mut aligns in read_to_aligns.values_mut() {
+        aligns.sort_by_key(&align_sort_key);
+    }
+    
+    let mut align_all_out = std::fs::File::create("read-aligns-all.txt")?;
+    let mut align_unique_out = std::fs::File::create("read-aligns-unique.txt")?;
+    
+    for (&ref read, &(ref barcode, ref library)) in read_to_barcode.iter() {
+        write!(align_all_out, "{}\t{}\t", barcode, library)?;
         if let Some(aligns) = read_to_aligns.get(read) {
+            let status = if aligns.len() == 1 { "Unique" } else { "Multi" };
+            let terse: Result<Vec<_>,_> = aligns.iter().map(|a| terse_align(&target_names, a)).collect();
+            write!(align_all_out, "{}\t{}\n", status, terse?.join("\t"))?;
+
             if aligns.len() == 1 {
-                write!(all_out, "{}\t{}\tUnique\n", barcode, library)?;
-                write!(
-                    good_out,
-                    "{}\t{}\t{}\n",
-                    barcode,
-                    library,
-                    format_align(&target_names, &aligns[0])?
-                )?;
-            } else {
-                write!(
-                    all_out,
-                    "{}\t{}\tMulti\t{}\n",
-                    barcode,
-                    library,
-                    aligns.len()
-                )?;
+                write!(align_unique_out,
+                       "{}\t{}\t{}\n",
+                       barcode, library,
+                       format_align(&target_names, &aligns[0])?)?;
             }
         } else {
-            write!(all_out, "{}\t{}\tNone\n", barcode, library)?;
+            write!(align_all_out, "None\n")?;
         }
     }
 
-    let mut assign_out = std::fs::File::create("barcode-assign-good.txt")?;
-    let mut barcode_out = std::fs::File::create("barcode-assign-all.txt")?;
+    let mut barcode_all_out = std::fs::File::create("barcode-assign-all.txt")?;
+    let mut barcode_unambig_out = std::fs::File::create("barcode-assign-unambig.txt")?;
+    let mut barcode_unique_out = std::fs::File::create("barcode-assign-unique.txt")?;
 
-    for (&(ref library, ref barcode), &ref reads) in barcode_to_reads.iter() {
+    for (&(ref barcode, ref library), &ref reads) in barcode_to_reads.iter() {
         let aligns: Vec<Vec<bam::Record>> = reads
             .iter()
             .map(|r| {
@@ -102,60 +99,40 @@ fn pacbio_join() -> Result<(), failure::Error> {
             })
             .collect();
 
-        if aligns.iter().any(|read_align| read_align.len() > 1) {
-            write!(
-                barcode_out,
-                "{}\t{}\t{}\tMulti\n",
-                library,
-                barcode,
-                reads.len()
-            )?;
+        write!(
+            barcode_all_out,
+            "{}\t{}\t{}\t",
+            barcode, library, reads.len())?;
+        
+        if is_ambiguous(&aligns) {
+            write!(barcode_all_out,
+                   "Ambig\t{}\n",
+                   format_ambiguous(&target_names, &aligns)?)?;
             continue;
         }
 
-        let read_aligns: Vec<&bam::Record> = aligns
-            .iter()
-            .filter_map(|read_align| read_align.first())
-            .collect();
+        let unambig = aligns.first().ok_or(failure::err_msg("Empty read set"))?;
 
-        if read_aligns.len() == 0 {
-            write!(
-                barcode_out,
-                "{}\t{}\t{}\tNone\n",
-                library,
-                barcode,
-                reads.len()
-            )?;
-            continue;
-        }
+        let status = if unambig.len() == 0 { "None" } else if unambig.len() > 1 { "Multi" } else { "Unique" };
 
-        if read_aligns
-            .iter()
-            .all(|ra| ra.tid() == read_aligns[0].tid() && ra.pos() == read_aligns[0].pos())
-        {
+        write!(barcode_all_out, "{}\n", status)?;
+        
+        let terse_res: Result<Vec<_>,_> = unambig.iter().map(|aln| terse_align(&target_names, aln)).collect();
+        
+        write!(
+            barcode_unambig_out,
+            "{}\t{}\t{}\t{}\t{}\n",
+            barcode, library, 
+            reads.len(), status,
+            terse_res?.join("\t"))?;
+
+        if unambig.len() == 1 {
             write!(
-                assign_out,
+                barcode_unique_out,
                 "{}\t{}\t{}\t{}\n",
-                library,
-                barcode,
+                barcode, library,
                 reads.len(),
-                format_align(&target_names, read_aligns[0])?
-            )?;
-            write!(
-                barcode_out,
-                "{}\t{}\t{}\tGood\n",
-                library,
-                barcode,
-                reads.len()
-            )?;
-        } else {
-            write!(
-                barcode_out,
-                "{}\t{}\t{}\tMixed\n",
-                library,
-                barcode,
-                reads.len()
-            )?;
+                format_align(&target_names, &unambig[0])?)?;
         }
     }
 
@@ -170,4 +147,47 @@ fn format_align(names: &Vec<String>, r: &bam::Record) -> Result<String, failure:
         r.cigar().end_pos()?,
         if r.is_reverse() { "-" } else { "+" }
     ))
+}
+
+fn terse_align(names: &Vec<String>, r: &bam::Record) -> Result<String, failure::Error> {
+    Ok(format!("{}:{}-{}({})",
+               names[r.tid() as usize],
+               r.pos(),
+               r.cigar().end_pos()?,
+               if r.is_reverse() { "-" } else { "+" }
+    ))               
+}
+
+fn align_sort_key(r: &bam::Record) -> (i32, i32) {
+    (r.tid(), r.pos())
+}
+
+const POS_TOL: i32 = 3;
+
+fn is_ambiguous(aligns: &Vec<Vec<bam::Record>>) -> bool {
+    fn align_equivalent(r0: &bam::Record, r1: &bam::Record) -> bool {
+        r0.tid() == r1.tid() && (r0.pos() - r1.pos()).abs() < POS_TOL && r0.is_reverse() == r1.is_reverse()
+    }
+
+    fn aligns_equivalent(alns0: &Vec<bam::Record>, alns1: &Vec<bam::Record>) -> bool {
+        alns0.len() == alns1.len() && alns0.iter().zip(alns1.iter()).all(|(r0, r1)| align_equivalent(r0, r1))
+    }
+    
+    let mut aligns_iter = aligns.iter();
+
+    if let Some(aligns0) = aligns_iter.next() {
+        aligns_iter.any(|aligns1| !aligns_equivalent(aligns0, aligns1))
+    } else {
+        false
+    }
+}
+
+fn format_ambiguous(names: &Vec<String>, aligns: &Vec<Vec<bam::Record>>) -> Result<String, failure::Error> {
+    fn format_read(names: &Vec<String>, alns: &Vec<bam::Record>) -> Result<String, failure::Error> {
+        let terse_res: Result<Vec<_>,_> = alns.iter().map(|aln| { terse_align(names, aln) }).collect();
+        Ok(terse_res?.join(";"))
+    }
+
+    let read_res: Result<Vec<_>,_> = aligns.iter().map(|alns| format_read(names, alns)).collect();
+    Ok(read_res?.join("\t"))
 }
