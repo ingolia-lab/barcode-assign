24c8963c401170697fe5a2c60916a3803ca0cf18
Nicholas Ingolia <nick@ingolia.org>
2019-08-16 12:25:46 -0700

    Abstracted type for sample count table

--- src/barcode_assign/tabulate.rs
+++ src/barcode_assign/tabulate.rs
@@ -22,11 +22,11 @@ impl CLI {
         let mut counts = Vec::new();
         
         for input in self.inputs.iter() {
-            let input_counts = Self::read_counts(&input)?;
+            let input_counts = SampleCounts::from_file(&input)?;
             counts.push((input.to_string(), input_counts));
         }
         
-        let total_counts = Self::total_read_counts(counts.iter().flat_map(|(_input, counts)| counts.iter()));
+        let total_counts = SampleCounts::total_counts(counts.iter().map(|(_input, counts)| counts));
         let mut barcodes: Vec<(String, usize)> = total_counts.into_iter().collect();
         barcodes.sort_by_key(|(_barcode, counts)| -(*counts as isize));
 
@@ -43,7 +43,7 @@ impl CLI {
         };
         
         for (barcode, _counts) in barcodes.iter() {
-            let count_vec = Self::barcode_count_vec(&counts, barcode);
+            let count_vec = SampleCounts::barcode_count_vec(counts.iter().map(|(_input, counts)| counts), barcode);
             
             if self.is_omitted(&count_vec) {
                 write!(omit_out, "{}\n", barcode)?;
@@ -83,47 +83,64 @@ impl CLI {
         false
     }
     
-    pub fn barcode_count_vec(counts: &Vec<(String, HashMap<String, usize>)>,
-                             barcode: &str)
-                             -> Vec<usize>
-    {
-        let mut cvec = Vec::new();
-        for (_input, input_counts) in counts.iter() {
-            cvec.push(input_counts.get(barcode).map_or(0, |ct| *ct));
-        }
-        cvec
+}
+
+pub struct SampleCounts (HashMap<String, usize>);
+
+impl SampleCounts {
+    pub fn from_file<P: AsRef<Path> + Display>(filename: P) -> Result<Self, failure::Error> {
+        Self::read(std::fs::File::open(filename.as_ref())?)
+            .map_err(|e| format_err!("Reading file {}: {}", filename, e))
     }
-    
-    pub fn total_read_counts<'a, I: Iterator<Item = (&'a String, &'a usize)>>(read_counts: I) -> HashMap<String, usize> {
-        let mut total_counts = HashMap::new();
-        for (barcode, count) in read_counts {
-            let barcode_count = total_counts.entry(barcode.to_string()).or_insert(0);
-            *barcode_count += *count;
-        }
-        total_counts
-    }
-    
-    pub fn read_counts<P: AsRef<Path> + Display>(input: P) -> Result<HashMap<String,usize>, failure::Error> {
+
+    pub fn read<R: Read>(input: R) -> Result<Self, failure::Error> {
         let mut counts = HashMap::new();
         
-        for (line_no, line_res) in BufReader::new(std::fs::File::open(input.as_ref())?).lines().enumerate() {
+        for (line_no, line_res) in BufReader::new(input).lines().enumerate() {
             let line = line_res?;
             let mut field_iter = line.split("\t");
 
             let barcode = field_iter
                 .next()
-                .ok_or_else(|| format_err!("Missing barcode in {} line {}", input, line_no))?;
+                .ok_or_else(|| format_err!("Missing barcode line {}", line_no))?;
             let count = field_iter
                 .next()
-                .ok_or_else(|| format_err!("Missing count in {} line {} barcode {}", input, line_no, barcode))?
+                .ok_or_else(|| format_err!("Missing count line {} barcode {}", line_no, barcode))?
                 .parse::<usize>()
-                .map_err(|e| format_err!("Malformed count in {} line {} barcode {}: {}", input, line_no, barcode, e))?;
+                .map_err(|e| format_err!("Malformed count line {} barcode {}: {}", line_no, barcode, e))?;
             if field_iter.next().is_some() {
-                bail!("Extra fields after count in {} line {} barcode {}\n{:?}", input, line_no, barcode, line);
+                bail!("Extra fields after count line {} barcode {}\n{:?}", line_no, barcode, line);
             }
             
             counts.insert(barcode.to_string(), count);
         }
-        Ok(counts)
+        Ok( SampleCounts(counts) )
+    }
+
+    pub fn total_counts<'a, I: Iterator<Item = &'a SampleCounts>>(counts_iter: I) -> Self {
+        let mut total_counts = HashMap::new();
+        for SampleCounts (count_map) in counts_iter {
+            for (barcode, count) in count_map.iter() {
+                let barcode_count = total_counts.entry(barcode.to_string()).or_insert(0);
+                *barcode_count += *count;
+            }
+        }
+        SampleCounts(total_counts)
+    }
+
+    pub fn barcode_count_vec<'a, I: Iterator<Item = &'a SampleCounts>>(counts_iter: I, barcode: &'a str) -> Vec<usize>
+    {
+        counts_iter.map(|SampleCounts(count_table)| count_table.get(barcode).map_or(0, |ct| *ct)).collect()
+    }
+    
+}
+
+impl IntoIterator for SampleCounts
+{
+    type Item = (String, usize);
+    type IntoIter = ::std::collections::hash_map::IntoIter<String, usize>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        self.0.into_iter()
     }
 }
